8bac908d8911798bb9bdd75892df23e357d9f0c4
salah <salahswidan212@gmail.com>
2024-12-05 20:13:12 +0200

    add fonts

--- lib/features/on_boarding/screens/on_boarding_screen.dart
+++ lib/features/on_boarding/screens/on_boarding_screen.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
 
+import '../../../core/resourses/color_managers.dart';
+import '../../splash/widgets/custom_logo_splash_screen.dart';
+
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+          child: Center(
+            child: Text(
+              "data",
+              style: TextStyle(
+                  fontFamily: "main", color: Colors.amber, fontSize: 30),
+            ),
+          ),
+          width: double.infinity,
+          height: double.infinity,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment(0.20, -0.98),
+                end: Alignment(-0.20, 0.98),
+                colors: [
+                  ColorManagers.kPrimaryColor,
+                  ColorManagers.kSecondaryColor
+                ]),
+          )),
+    );
   }
 }
