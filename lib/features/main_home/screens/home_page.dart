ea59163c4e0ac9167b4a8972099c325aac0faf05
salah <salahswidan212@gmail.com>
2024-12-07 16:00:23 +0200

    add Gestured on tap to custom search details

--- lib/features/main_home/screens/home_page.dart
+++ lib/features/main_home/screens/home_page.dart
@@ -1,7 +1,10 @@
+import 'package:audio_player_app/features/main_home/widgets/custom_recently_sourahs_card.dart';
+import 'package:audio_player_app/models/soura_model.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../../core/resourses/color_managers.dart';
-import '../widgets/custom_recently_player_list_view.dart';
+import '../widgets/custom_search_details.dart';
 import '../widgets/custom_text_field_home_page.dart';
 import '../widgets/custom_title_search_home_page.dart';
 
@@ -19,24 +22,39 @@ class HomePage extends StatelessWidget {
             ColorManagers.kPrimaryColor,
             ColorManagers.kDarkBlueeColor
           ])),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomTextFieldHomePage(),
-          CustomTitleSearchHomePage(
-            title: "Recently Played",
-          ),
-          SizedBox(
-            height: 22,
-          ),
-          CustomRecentlyPlayerListView(
-            readerName: "Adeel",
-            souraName: "Al-Baqarah",
-          ),
-          CustomTitleSearchHomePage(
-            title: "Recently Sourahs",
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CustomTextFieldHomePage(),
+            CustomTitleSearchHomePage(
+              title: "Recently Played",
+            ),
+            SizedBox(
+              height: 22,
+            ),
+            CustomSearchDetails(
+              itemCount: 3,
+              souraModel: SouraModel(
+                  image: "assets/images/quran_image/qu3.jpg",
+                  reader: "Salah",
+                  soura: "Al-Baqarah",
+                  pathSoura: "pathSoura"),
+            ),
+            CustomTitleSearchHomePage(
+              title: "Recommended Sourahs",
+            ),
+            ListView.separated(
+              separatorBuilder: (context, index) => SizedBox(
+                height: 10,
+              ),
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemBuilder: (context, index) => CustomReceentlySourahsCard(),
+              itemCount: 10,
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/features/main_home/widgets/custom_search_details.dart
@@ -0,0 +1,63 @@
+import 'package:audio_player_app/models/soura_model.dart';
+import 'package:flutter/material.dart';
+
+class CustomSearchDetails extends StatelessWidget {
+  CustomSearchDetails({
+    super.key,
+    required this.souraModel,
+    required this.itemCount,
+  });
+  final SouraModel souraModel;
+  final int itemCount;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 198,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 22.0),
+        child: ListView.separated(
+            //shrinkWrap: true,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) => GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          child: Image.asset(
+                            "assets/images/quran_image/qu3.jpg",
+                            fit: BoxFit.cover,
+                            width: 151,
+                            height: 151,
+                          ),
+                          //child: Image.asset("assets/images/quran_image/qu3.jpg"),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        Text(
+                          souraModel.soura,
+                          style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white),
+                        ),
+                        SizedBox(
+                          height: 9,
+                        ),
+                        Text(
+                          souraModel.reader,
+                          style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w500,
+                              color: Color(0xffB1AFE9)),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+            separatorBuilder: (context, index) => SizedBox(width: 17),
+            itemCount: itemCount),
+      ),
+    );
+  }
+}
